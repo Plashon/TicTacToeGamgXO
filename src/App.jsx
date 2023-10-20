49884679516188c53bbd567efcf94f5748e62b08
Plashon <fourza12poobate@gmail.com>
2023-10-20 12:14:33 +0700

    frist commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import TicTacToe from './components/TicTacToe'
+ 
+
+function App() {
+
+  return (
+    <>
+      <TicTacToe/>
+    </>
+  )
+}
+
+export default App
